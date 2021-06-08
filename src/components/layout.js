61da8476b4799277315186f14ba60d4b29aa7798
veryspry <ehlinger.matt@gmail.com>
2021-06-08 16:42:41 -0500

    succeed

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Heade siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
